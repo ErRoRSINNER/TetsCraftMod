0e0eeb0619631c879f85ff9661dfd5c3da6d9a0e
BuildTools <unconfigured@null.spigotmc.org>
2025-05-01 19:16:29 +0200

    U2

--- src/content/blocks/Blocks.java
+++ src/content/blocks/Blocks.java
@@ -1,21 +1,39 @@
 package content.blocks;
 
 import mindustry.content.Items;
+import mindustry.content.Liquids;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
 import mindustry.world.Block;
 import mindustry.world.blocks.production.Drill;
 
 public class Blocks{
-    public static Block gomodrill, tetsDrill;
+    public static Block homoDrill, miniDrill, tetsDrill;
 
 
     public static void load() {
-        gomodrill = new Block("gomodrill") {
+        homoDrill = new Drill("homo-drill") {
             {
-                rotate = false;
+                requirements(Category.production, ItemStack.with(Items.copper, 24));
+                tier = 2;
+                this.drillTime = 500.0F;
                 size = 1;
-                health = 200;
+                this.researchCost = ItemStack.with(new Object[]{Items.copper, 100});
+                this.consumeLiquid(Liquids.water, 0.06F).boost();
+
+                health = 100;
+            }
+        };
+        miniDrill = new Drill("mini-drill") {
+            {
+                requirements(Category.production, ItemStack.with(Items.copper, 24, Items.lead, 24));
+                tier = 2;
+                this.drillTime = 400.0F;
+                size = 1;
+                this.researchCost = ItemStack.with(new Object[]{Items.copper, 100, Items.lead, 60});
+                this.consumeLiquid(Liquids.water, 0.06F).boost();
+
+                health = 120;
             }
         };
 
