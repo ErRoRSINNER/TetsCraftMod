e2d5ccef193c707406ca68a93b456ce47f975461
BuildTools <unconfigured@null.spigotmc.org>
2025-05-01 19:22:21 +0200

    U2

--- src/content/blocks/Blocks.java
+++ src/content/blocks/Blocks.java
@@ -8,35 +8,40 @@ import mindustry.world.Block;
 import mindustry.world.blocks.production.Drill;
 
 public class Blocks{
-    public static Block homoDrill, miniDrill, tetsDrill;
+    public static Block homoDrill, miniDrill, nihonDrill, tetsDrill;
 
 
     public static void load() {
-        homoDrill = new Drill("homo-drill") {
-            {
-                requirements(Category.production, ItemStack.with(Items.copper, 24));
+        homoDrill = new Drill("homo-drill") {{
+                //requirements(Category.production, ItemStack.with(Items.copper, 24)); TODO
                 tier = 2;
                 this.drillTime = 500.0F;
                 size = 1;
-                this.researchCost = ItemStack.with(new Object[]{Items.copper, 100});
+                //this.researchCost = ItemStack.with(new Object[]{Items.copper, 100});
                 this.consumeLiquid(Liquids.water, 0.06F).boost();
 
                 health = 100;
-            }
-        };
-        miniDrill = new Drill("mini-drill") {
-            {
-                requirements(Category.production, ItemStack.with(Items.copper, 24, Items.lead, 24));
+        }};
+        miniDrill = new Drill("mini-drill") {{
+                //requirements(Category.production, ItemStack.with(Items.copper, 24, Items.lead, 24)); TODO
                 tier = 2;
                 this.drillTime = 400.0F;
                 size = 1;
-                this.researchCost = ItemStack.with(new Object[]{Items.copper, 100, Items.lead, 60});
-                this.consumeLiquid(Liquids.water, 0.06F).boost();
+                //this.researchCost = ItemStack.with(new Object[]{Items.copper, 100, Items.lead, 60});
+                this.consumeLiquid(Liquids.water, 0.08F).boost();
 
                 health = 120;
-            }
-        };
+        }};
+        nihonDrill = new Drill("nihon-drill") {{
+                //requirements(Category.production, ItemStack.with(Items.copper, 24, Items.lead, 24)); TODO
+                tier = 3;
+                this.drillTime = 200.0F;
+                size = 1;
+                //this.researchCost = ItemStack.with(new Object[]{Items.copper, 100, Items.lead, 60});
+                this.consumeLiquid(Liquids.water, 0.1F).boost();
 
+                health = 180;
+        }};
         tetsDrill = new Drill("tets-drill") {{
             requirements(Category.production, ItemStack.with(Items.thorium, 12));
             tier = 5;
