391e322a18c4fc64f917291eefb90484040fba3a
BuildTools <unconfigured@null.spigotmc.org>
2025-05-02 12:44:57 +0200

    Добавил забытые предметы, обновил цвета

--- src/content/items/TItems.java
+++ src/content/items/TItems.java
@@ -5,7 +5,7 @@ import mindustry.type.Item;
 
 public class TItems {
     public static Item battery, bee, beeq, capsule_of_oxigen, capsule_of_hydrogen, concrete, crystal, expl_mixture, goddamm_ingot, hyperalloy, low_quality_crystal,
-            M_13_5, mica, neoch_pravos, nihonium, pingwan, poop, pravoslaviy, RM_20_17, superconductor, tantalium, tets_ingot, toxium, vermillion;
+            M_13_5, mica, neoch_pravos, nihonium, pingwan, poop, pravoslaviy, reinforced_glass, RM_20_17, superconductor, tantalium, tets_ingot, toxium, vermillion;
 
     public static void load() {
         battery = new Item("battery", Color.valueOf("9badb7")) {{
@@ -18,12 +18,12 @@ public class TItems {
         beeq = new Item("beeq", Color.valueOf("fdaa00")) {{
             cost = 1.5f;
         }};
-        capsule_of_oxigen = new Item("capsule_of_oxigen") {{
+        capsule_of_oxigen = new Item("capsule_of_oxigen", Color.valueOf("#6dd5f8")) {{
             flammability = 3;
             explosiveness = 1;
             buildable = false;
         }};
-        capsule_of_hydrogen = new Item("capsule_of_hydrogen") {{
+        capsule_of_hydrogen = new Item("capsule_of_hydrogen", Color.valueOf("#0992f6")) {{
             flammability = 6;
             explosiveness = 1.5f;
             buildable = false;
@@ -37,72 +37,78 @@ public class TItems {
             hardness = 2;
             cost = 2;
         }};
-        expl_mixture = new Item("expl_mixture") {{
+        expl_mixture = new Item("expl_mixture", Color.valueOf("#006fff")) {{
             flammability = 10;
             explosiveness = 5;
             buildable = false;
         }};
-        goddamm_ingot = new Item("goddamm_ingot") {{
+        goddamm_ingot = new Item("goddamm_ingot", Color.valueOf("#f9ff13")) {{
             cost = 6;
             radioactivity = 2;
         }};
-        hyperalloy = new Item("hyperalloy") {{
+        hyperalloy = new Item("hyperalloy", Color.valueOf("#bdff00")) {{
             cost = 5.5f;
             radioactivity = 0.2f;
             charge = 1;
         }};
-        low_quality_crystal =
-        M_13_5 = new Item("M_13_5") {{
+        low_quality_crystal = new Item("low_quality_crystal", Color.valueOf("ffffff")) {{
+            cost = 1;
+        }};
+        M_13_5 = new Item("M_13_5", Color.valueOf("#e5e7e6")) {{
             radioactivity = 0.05f;
             buildable = false;
         }};
-        mica = new Item("mica") {{
+        mica = new Item("mica", Color.valueOf("#9CA7A7")) {{
             cost = 0.4f;
             charge = 0.01f;
         }};
-        neoch_pravos = new Item("neoch_pravos") {{
+        neoch_pravos = new Item("neoch_pravos", Color.valueOf("#C1C43B")) {{
             hardness = 3;
             radioactivity = 0.1f;
             buildable = false;
         }};
-        nihonium = new Item("nihonium", Color.valueOf("DAB1DC")) {{
+        nihonium = new Item("nihonium", Color.valueOf("#DAB1DC")) {{
             cost = 7;
             radioactivity = 4f;
             healthScaling = 4f;
         }};
-        pingwan =  new Item("pingwan") {{
+        pingwan =  new Item("pingwan", Color.valueOf("#A8A7A6")) {{
             buildable = false;
         }};
-        poop = new Item("poop") {{
+        poop = new Item("poop", Color.valueOf("#CD9232")) {{
             buildable = false;
         }};
-        pravoslaviy = new Item("pravoslaviy") {{
+        pravoslaviy = new Item("pravoslaviy", Color.valueOf("#FFFCA3")) {{
             cost = 3;
             radioactivity = 0.5f;
         }};
-        RM_20_17 = new Item("RM_20_17") {{
+        reinforced_glass = new Item("reinforced_glass", Color.valueOf("#82B5FF")) {{
+            cost = 3;
+            radioactivity = 0.1f;
+        }};
+        RM_20_17 = new Item("RM_20_17", Color.valueOf("#FF5049")) {{
             cost = 2;
             radioactivity = 1.2f;
             flammability = 1.6f;
             explosiveness = 1.1f;
             healthScaling = -1f;
         }};
-        superconductor = new Item("superconductor") {{
+        superconductor = new Item("superconductor", Color.valueOf("#FBD504")) {{
             cost = 1.2f;
             charge = 0.2f;
         }};
-        tantalium = new Item("tantalium") {{
+        tantalium = new Item("tantalium", Color.valueOf("#989195")) {{
             cost = 1.01f;
         }};
-        tets_ingot = new Item("tets_ingot") {{
+        tets_ingot = new Item("tets_ingot", Color.valueOf("#38EDD7")) {{
             cost = 1.98f;
         }};
-        toxium = new Item("toxium") {{
+        toxium = new Item("toxium", Color.valueOf("#F565CE")) {{
             cost = -1.1f;
             flammability = 2;
             explosiveness = 3;
         }};
-        vermillion = new Item("vermillion") {{
+        vermillion = new Item("vermillion", Color.valueOf("#F40B34")) {{
             buildable = false;
         }};
     }
